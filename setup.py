dc3f19a4f379ac930b1677a63dcab64e0887a320
Matthew Rankin <matthew@questrail.com>
2017-07-24 20:15:47 -0500

    Add pypi required files.

---
+++ setup.py
@@ -0,0 +1,69 @@
+import codecs
+import os
+import re
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """Read parts of a file
+
+    Taken from pip's setup.py
+    intentionally *not* adding an encoding option to open
+    see: https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    """
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+def find_version(*file_paths):
+    """Find version in source file
+
+    Read the version number from a source file.
+    Code taken from pip's setup.py
+    """
+    version_file = read(*file_paths)
+    # The version line must have the form:
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='cableloss',
+    version=find_version('cableloss.py'),
+    author='Matthew Rankin',
+    author_email='matthew@questrail.com',
+    py_modules=['cableloss'],
+    url='http://github.com/questrail/cableloss',
+    license='MIT',
+    description='Calculate cable loss based on cable type'
+        + 'and length',
+    long_description=long_description,
+    requires=['numpy (>=1.6.0)'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
